20d98ff17eec3baf7d26423e7ac66b0be328a490
Samuel McNally <sammcnally785@gmail.com>
2026-02-06 17:45:27 -0600

    parser files (forgot to add)

---
+++ src/parsing/parser.c
@@ -0,0 +1,5 @@
+//
+// Created by sammc on 2/6/26.
+//
+
+#include "parser.h"
---
+++ src/parsing/parser.h
@@ -0,0 +1,8 @@
+//
+// Created by sammc on 2/6/26.
+//
+#pragma once
+#ifndef EXPRESC_PARSER_H
+#define EXPRESC_PARSER_H
+
+#endif //EXPRESC_PARSER_H
